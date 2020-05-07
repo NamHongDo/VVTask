aa845c83435b599b7f793a1bb4dfaae12a7b5410
Nam Do <sirsunday89@gmail.com>
2020-05-07 11:52:50 -0400

    Implement Login View and Login functionality in Account Controller

--- VVTask/Controllers/AccountController.cs
+++ VVTask/Controllers/AccountController.cs
@@ -56,5 +56,31 @@ namespace VVTask.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("index", "home");
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Email, 
+                    model.Password, 
+                    model.RememberMe, 
+                    false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                ModelState.AddModelError(string.Empty,"Invalid Login Attempt");
+            }
+            return View(model);
+        }
+
     }
 }
